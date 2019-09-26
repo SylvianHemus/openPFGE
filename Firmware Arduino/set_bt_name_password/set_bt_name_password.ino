c0da2a35146199ed225ba27fe40bb122953df0af
diegusleik <diegolagos@gmail.com>
2019-09-26 09:25:42 -0300

    Firmware & circuit updates

---
+++ Firmware Arduino/set_bt_name_password/set_bt_name_password.ino
@@ -0,0 +1,41 @@
+// Thanks to:
+// RoboIndia Code for HC-05 with AT Mode
+// https://www.roboindia.com/tutorials
+
+#include <SoftwareSerial.h>
+
+SoftwareSerial BT(10, 11); // TX, RX
+
+#define BTName openPFGE
+#define BTPass 1234
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("Enter AT commands:");
+  BT.begin(38400);
+  if (Serial.available()){
+    BTSerial.print("AT+NAME=");
+    BTSerial.println(BTName);
+    BTSerial.print("AT+PSWD=");
+    BTSerial.println(BTPass);
+    
+    Serial.println("Name & Password set");
+        
+    Serial.print("Name: ");
+    BTSerial.println("AT+NAME");
+    Serial.println(BTSerial.read());
+    
+    Serial.println("Password:");
+    BTSerial.println("AT+PSWD");
+    Serial.println(BTSerial.read());
+    
+    Serial.println("Version:");
+    BTSerial.println("AT+VERSION");
+    Serial.println(BTSerial.read());
+  } else {
+    Serial.println("BT not available");
+    }
+}
+
+void loop(){}
